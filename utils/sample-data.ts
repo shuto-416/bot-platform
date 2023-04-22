ab9c5053cfe4675b9f1fccee8596fc404dba68ca
shuto-416 <hsjudo416@gmail.com>
2023-04-22 17:15:15 +0900

    その他微修正

--- utils/sample-data.ts
+++ utils/sample-data.ts
@@ -1,9 +1,9 @@
 import { User } from '../interfaces'
 
 /** Dummy user data. */
-export const sampleUserData: User[] = [
-  { id: 101, name: 'Alice' },
-  { id: 102, name: 'Bob' },
-  { id: 103, name: 'Caroline' },
-  { id: 104, name: 'Dave' },
+export const sampleUserData: Array<User> = [
+  { id: 101, name: 'Alice', chats: [{ text: 'hello', sender: 'you' }, { text: 'hello', sender: 'me' }] },
+  { id: 102, name: 'Bob', chats: [{ text: 'yoyo', sender: 'you' }, { text: 'yo, check it out yo.', sender: 'me' }] },
+  { id: 103, name: 'Caroline', chats: [{ text: 'Whats up dude?', sender: 'you' }, { text: 'yeeeah, im goood!', sender: 'me' }] },
+  { id: 104, name: 'Dave', chats: [{ text: 'Are you happy? Do you trust GOD?', sender: 'you' }, { text: 'No,,please,,NO,,!!', sender: 'me' }] },
 ]
