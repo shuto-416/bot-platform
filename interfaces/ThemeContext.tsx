467df8c503c6a1880b98b354caefbc5f1d2baf0b
shuto-416 <hsjudo416@gmail.com>
2023-04-22 17:13:48 +0900

    テーマ作成

---
+++ interfaces/ThemeContext.tsx
@@ -0,0 +1,31 @@
+import React, { createContext, useContext, useState } from 'react'
+
+type Theme = 'light' | 'dark'
+
+interface ThemeContextValue {
+  theme: Theme
+  toggleTheme: () => void
+}
+
+const ThemeContext = createContext<ThemeContextValue>({
+  theme: 'light',
+  toggleTheme: () => {},
+})
+
+const ThemeProvider: React.FC = ({ children }) => {
+  const [theme, setTheme] = useState<Theme>('light')
+
+  const toggleTheme = () => {
+    setTheme((prevTheme) => (prevTheme === 'light' ? 'dark' : 'light'))
+  }
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  )
+}
+
+export const useTheme = () => useContext(ThemeContext)
+
+export default ThemeProvider
